fc6dc199239eb8dbfbe5522fada581937f2631c2
Vladimir Botka <vbotka@gmail.com>
2026-06-19 15:24:23 +0200

    Update ansible-pull-syslogng-server and client in post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -5,9 +5,9 @@ ansible_custom_facts_dir="/etc/ansible/facts.d"
 
 case "$plugin_name" in
     ansible-syslogng)
-	# Create custom facts
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        # Create custom facts
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -15,21 +15,21 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	# Ansible needs UTF-8
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	# Configure and start syslog-ng server
-	mkdir /root/ansible-conf-syslogng-server
-	cd /root/ansible-conf-syslogng-server
-	ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-syslogng-server.git -d /root/ansible-conf-syslogng-server /root/ansible-conf-syslogng-server/pb-logserv.yml
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        # Ansible needs UTF-8
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        # Configure and start syslog-ng server
+        mkdir /root/ansible-conf-syslogng-server
+        cd /root/ansible-conf-syslogng-server
+        ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-syslogng-server.git -d /root/ansible-conf-syslogng-server /root/ansible-conf-syslogng-server/pb-logserv.yml
+        ;;
     ansible-test)
-	cat << EOF > /root/PLUGIN_INFO
+        cat << EOF > /root/PLUGIN_INFO
 plugin_name: $plugin_name
 plugin_ip: $IOCAGE_PLUGIN_IP
 EOF
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -37,13 +37,13 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        ;;
     ansible-pull-init)
-	# Create custom facts
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        # Create custom facts
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -51,18 +51,18 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	# Ansible needs UTF-8
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	# Init
-	mkdir /root/ansible-conf-init
-	cd /root/ansible-conf-init
-	ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-init.git -d /root/ansible-conf-init pb-init.yml
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        # Ansible needs UTF-8
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        # Init
+        mkdir /root/ansible-conf-init
+        cd /root/ansible-conf-init
+        ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-init.git -d /root/ansible-conf-init pb-init.yml
+        ;;
     ansible-pull-syslogng-client)
-	# Create custom facts
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        # Create custom facts
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -70,18 +70,22 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	# Ansible needs UTF-8
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	# Configure syslog-ng client
-	mkdir /root/ansible-conf-syslogng-client
-	cd /root/ansible-conf-syslogng-client
-	ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-syslogng-client.git -d /root/ansible-conf-syslogng-client pb-logclient.yml
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        # Ansible needs UTF-8
+        echo "LANG=en_US.UTF-8" >> /root/.profile
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        # Configure syslog-ng client
+        ansible-pull \
+            -i hosts \
+            -U https://github.com/vbotka/ansible-conf-syslogng-client.git \
+            -d /root/ansible-conf-syslogng-client \
+            -e "ansible_pull_mode=true" \
+            pb-logclient.yml
+        ;;
     ansible-pull-syslogng-server)
-	# Create custom facts
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        # Create custom facts
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -89,17 +93,21 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	# Ansible needs UTF-8
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	# Configure syslog-ng server
-	mkdir /root/ansible-conf-syslogng-server
-	cd /root/ansible-conf-syslogng-server
-	ansible-pull -i hosts -U https://github.com/vbotka/ansible-conf-syslogng-server.git -d /root/ansible-conf-syslogng-server pb-logserv.yml
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        # Ansible needs UTF-8
+        echo "LANG=en_US.UTF-8" >> /root/.profile
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        # Configure syslog-ng server
+        ansible-pull \
+            -i hosts \
+            -U https://github.com/vbotka/ansible-conf-syslogng-server.git \
+            -d /root/ansible-conf-syslogng-server \
+            -e "ansible_pull_mode=true" \
+            pb-logserv.yml
+        ;;
     ansible-pull-test)
-	mkdir -p "$ansible_custom_facts_dir"
-	cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
+        mkdir -p "$ansible_custom_facts_dir"
+        cat << EOF2 > "${ansible_custom_facts_dir}/iocage.fact"
 #!/bin/sh
 cat << EOF
 {
@@ -107,9 +115,9 @@ cat << EOF
 }
 EOF
 EOF2
-	chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
-	echo "LC_ALL=en_US.UTF-8" >> /root/.profile
-	;;
+        chmod a+x "${ansible_custom_facts_dir}/iocage.fact"
+        echo "LC_ALL=en_US.UTF-8" >> /root/.profile
+        ;;
     ansible-zero)
-	;;
+        ;;
 esac
